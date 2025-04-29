c973c8d652fe3d30d180d6627860cf3a76d7b845
ameybhosle212 <ameybhosle212@gmail.com>
2025-04-29 23:34:28 +0530

    added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Dashboard from './Components/Dashboard';
 import Login from './Components/Login'
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
     </>
---
+++ src/Components/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Go to /login or /dashboard</h1>
+        </div>
+    );
+}
+
+export default HomePage;
